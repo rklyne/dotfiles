c4c1a868726fa0d2d885813c293b9b05d22db9ab
Ronan Klyne <github@rklyne.net>
2013-06-21 14:48:57 +0100

    web install script plus tmux tweaks

---
+++ webinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+ 
+# script to install these scripts on a new host straight from github.
+
+# Install command:
+#
+#
+
+cd
+git clone https://github.com/rklyne/dotfiles.git
+cd dotfiles
+#chmod +x ./install.sh
+./install.sh
+
