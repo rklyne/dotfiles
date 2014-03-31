b0e7fbfbfcaacc3e8434d142c158dc9d21b6fb0a
Ronan Klyne <github@rklyne.net>
2014-03-31 17:52:21 +0100

    Update install script to install bundles

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 ln -s "${PWD}/tmux.conf" ~/.tmux.conf
 ln -s "${PWD}/vimrc" ~/.vimrc
+ln -s "${PWD}/vim" ~/.vim
 ln -s "${PWD}/gitconfig" ~/.gitconfig
+
+(cd ~/.vim && ./install_bundles.sh)
 
 
--- vim/install_bundles.sh
+++ vim/install_bundles.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+mkdir bundle
 for r in `cat repos`; do (cd bundle; git clone $r) ; done
 
